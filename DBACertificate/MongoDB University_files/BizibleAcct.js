272e86287966677a80198240cb0d897910c01a2c
Yi Zhao <zhaoyi0113@gmail.com>
2018-02-04 20:09:18 +1100

    add dba certification study guide"

---
+++ DBACertificate/MongoDB University_files/BizibleAcct.js
@@ -0,0 +1,12 @@
+(function () {
+    BizTrackingA.GoAccount({
+        webToLeadField: '',
+        chatEnabled: 'true' === 'true',
+        XUserId: 'a67f949595c848a4e95be605833fb429',
+        formProviderEnabled: 'true' === 'true',
+        attach_secure_forms: 'true' === 'true',
+        viewThroughEnabled: 'false' === 'true',
+        interceptAjax: 'true' === 'true'
+    });
+})();
+;
